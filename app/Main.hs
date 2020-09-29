f2889bbadaa97d4c0774bee2730ecde49638adc6
JEphron <jephron63@gmail.com>
2020-09-29 01:36:51 -0700

    Colors!

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns        #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE TemplateHaskell       #-}
 
@@ -13,18 +14,20 @@ import qualified Brick.Widgets.Center   as C
 import qualified Brick.Widgets.Dialog   as Dialog
 import           Brick.Widgets.Edit     (Editor)
 import qualified Brick.Widgets.Edit     as Editor
-import qualified Client
+import           Client
 import           Control.Monad          (join, void)
 import           Control.Monad.IO.Class (liftIO)
 import           Data.Maybe             (isNothing)
 import           Data.Text              (Text)
 import qualified Data.Text              as T
+import           Graphics.Vty           (Attr, black, blue, bold, cyan, green,
+                                         red, standout, underline, white,
+                                         withStyle, withURL, yellow)
 import qualified Graphics.Vty           as V
 import           Lens.Micro.Platform
 import           Network.URI            (URI)
 import qualified Network.URI            as URI
 import qualified Output
-
 data Loadable a = NotStarted | Loading | Loaded a deriving (Show)
 
 data State = State
@@ -46,7 +49,15 @@ app = App { appDraw = drawUI
           }
 
 myAttrMap :: AttrMap
-myAttrMap = attrMap V.defAttr []
+myAttrMap = attrMap V.defAttr [ ("geminiH1", fg white `withStyle` bold `withStyle` underline)
+                              , ("geminiH2", fg white `withStyle` underline)
+                              , ("geminiH3", fg white `withStyle` bold)
+                              , ("geminiQuote", fg white)
+                              , ("geminiUri", fg yellow)
+                              , ("geminiPre", fg blue)
+                              , ("geminiUl", fg white)
+
+                              ]
 
 handleEvent :: State -> BrickEvent Name Event -> EventM Name (Next State)
 handleEvent s (VtyEvent (V.EvKey (V.KEnter) [])) = liftIO (doFetch s) >>= continue
@@ -64,7 +75,7 @@ doFetch s = do
     Nothing ->
       return s
 
-responseToState :: State -> Maybe Client.GeminiResponse -> State
+responseToState :: State -> Maybe GeminiResponse -> State
 responseToState s (Just geminiResponse@(Client.GeminiResponse uri _)) =
   s { _geminiContent = Loaded geminiResponse, _currentURI = Just uri}
 responseToState s Nothing = s
@@ -76,7 +87,38 @@ drawUI s =
       vBox [urlEditor, B.hBorder, outputVp, B.hBorder, drawStatusLine s]
   ]
   where urlEditor = (Editor.renderEditor (str . unlines) True (_urlEditor s))
-        outputVp = viewport ContentVP Vertical $ str (debugGeminiContent s)
+        outputVp = viewport ContentVP Vertical $
+          drawLoadable (_geminiContent s) drawGeminiContent
+
+drawLoadable :: Loadable a -> (a -> Widget Name) -> Widget Name
+drawLoadable NotStarted _    = str "Not Started"
+drawLoadable Loading _       = str "Loading..."
+drawLoadable (Loaded a) draw = draw a
+
+drawGeminiContent :: GeminiResponse -> Widget Name
+drawGeminiContent (GeminiResponse uri (Success (GeminiContent GeminiPage {pageLines}))) =
+  vBox (map displayLine pageLines)
+drawGeminiContent (GeminiResponse uri resp) = str (show resp)
+
+displayLine :: Line -> Widget Name
+displayLine (HeadingLine H1 t)  = withAttr "geminiH1" $ txt t
+displayLine (HeadingLine H2 t)  = withAttr "geminiH2" $ txt t
+displayLine (HeadingLine H3 t)  = withAttr "geminiH3" $ txt t
+displayLine (TextLine t)        = withAttr "geminiText" $ txt t
+displayLine (ULLine t)          = withAttr "geminiUl" $ txt ("• " <> t)
+displayLine (PreLine t)         = withAttr "geminiPre" $ txt t
+displayLine (QuoteLine t)       = withAttr "geminiQuote" $ txt ("┆ " <> t)
+displayLine (LinkLine uri desc) = displayLink uri desc
+
+displayLink :: Either Text URI -> Text -> Widget Name
+displayLink uriOrErr desc =
+  txt desc <+> str " " <+> uriBit
+  where uriBit = case uriOrErr of
+                    Right uri -> str "(" <+> (showUri uri) <+> str ")"
+                    Left  foo -> txt ("<URI parse failed!> - " <> foo)
+        uriToTxt uri = T.pack $ URI.uriToString id uri ""
+        showUri uri = modifyDefAttr (`withURL` (uriToTxt uri)) $
+                         withAttr "geminiUri" $ txt (uriToTxt uri)
 
 drawStatusLine :: State -> Widget Name
 drawStatusLine s = padRight Max status <+> help
